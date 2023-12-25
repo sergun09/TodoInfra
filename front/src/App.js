81418e180b685efe8aee5c2d39fc7878a92f2bfc
Kévin <kevin.legrand1@etu.univ-orleans.fr>
2023-12-26 00:29:18 +0100

    [Front] Ajout des fonctionnalités de suppression et modification de notes

--- front/src/App.js
+++ front/src/App.js
@@ -1,52 +1,81 @@
 import './App.css';
 import React, {useEffect, useState} from "react";
-import Note from "./components/Note/Note";
 import Service from "./Service";
-import Tableau from "./components/Tableau/Tableau";
+import Note from "./components/Note/Note";
 
 function App() {
 
     //state
     const [notes, setNotes] = useState([]);
-    const [isMounted, setIsMounted] = useState(false);
+    const [isMounted] = useState(false);
+    const [addNoteDisplayed, setAddNoteDisplayed] = useState(false)
+
     //behavior
     useEffect(() => {
         if (!isMounted) {
-            updateData();
+            Service.retrieveData().then(notes =>
+                setNotes(notes)
+            );
         }
     }, [isMounted]);
-    const updateData = () => {
 
-        Service.retrieveData().then(response => {
-            setNotes(response);
-            setIsMounted(true)
-        })
 
-    }
-
-    const insertData = (data) =>{
+    const addNote = (data) => {
         Service.commitData(data).then(
-            ()=> updateData()
+            () => {
+                Service.retrieveData().then(notes =>
+                    setNotes(notes))
+            }
         )
+        setAddNoteDisplayed(false)
+
+
     }
+
+    const deleteNote = (id) => {
+        Service.deleteData(id).then(
+            () => {
+                Service.retrieveData().then(notes =>
+                    setNotes(notes))
+            }
+        )
+
+    }
+
+    const updateNote = (id, data) => {
+        Service.updateData(id, data).then(
+            () => {
+                Service.retrieveData().then(notes =>
+                    setNotes(notes))
+            })
+        }
+
 
     //render
     return (
         <div className={"App"}>
             <h1 className={"nomApp"}>APP NOTE</h1>
-
             <div className={"container"}>
                 {notes.map((note) => (
-                    <Note key={note.id} noteInfo={note} readOnly={true}/>
+                    <div>
+                        <Note key={note.id} noteInfo={note} readOnly={true} methods={{update: updateNote, create: addNote, delete: deleteNote}}/>
+                    </div>
                 ))}
             </div>
-            <Tableau
-                name={"+"}
-                component={
-                    <Note readOnly={false} ajouterNote = {insertData}/>
-                }
-                className={"createNote"}
-            />
+
+            <div className={"createNote"}>
+                <div style={{visibility: addNoteDisplayed?"visible":"hidden"}}>
+                    <Note readOnly={false} methods={{create: addNote}} />
+                </div>
+                <button
+                    className={"add-note-button"}
+                    onClick={() => {
+                        setAddNoteDisplayed(!addNoteDisplayed)
+                    }}
+                >
+                    +
+                </button>
+            </div>
         </div>
     );
 }
--- front/src/Service.js
+++ front/src/Service.js
@@ -17,11 +17,29 @@ function Service() {
             }
         }).then(res => res.json())
     }
+    const updateData = (id, data) =>{
+        return fetch(apiURI + "/posts/"+id, {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers : {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        }).then(res => res.json())
+    }
+
+    const deleteData = (id) =>{
+        return fetch(apiURI + "/posts/"+id, {
+            method: "DELETE",
+            headers : {
+                "Content-Type": "application/json",
+            }
+        })
+    }
+
     return {
-        retrieveData,commitData
+        retrieveData,commitData,updateData, deleteData
     }
 }
-
-
 
 export default Service();
